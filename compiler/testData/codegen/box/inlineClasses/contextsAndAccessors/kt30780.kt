98cab4fdaeecdc400b37ed5b23f0f570d12dca76
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2019-04-04 16:56:19 +0300

    KT-30780 Fix erased inline class context in class context hierarchy
    In SourceCompilerForInline we could enter ERASED_INLINE_CLASS context
    from containing declaration context. That broke codegen context
    hierarchy invariants assumed in accessor generation.

---
+++ compiler/testData/codegen/box/inlineClasses/contextsAndAccessors/kt30780.kt
@@ -0,0 +1,13 @@
+inline class Test(val x: Int) {
+    private companion object {
+        private const val CONSTANT = "OK"
+    }
+
+    fun crash() = getInlineConstant()
+
+    private inline fun getInlineConstant(): String {
+        return CONSTANT
+    }
+}
+
+fun box() = Test(1).crash()
